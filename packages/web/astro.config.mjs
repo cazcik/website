879fef9ce2d3ba50a0a5b097ee34deced1f6c099
Zac Wojcik <zac.wojcik@gmail.com>
2024-08-28 15:17:47 -0400

    wire up blog

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,9 +1,13 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cazcik.com',
-  integrations: [tailwind({ applyBaseStyles: false})]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx(), sitemap()]
 });
