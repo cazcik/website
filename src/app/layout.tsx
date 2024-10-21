6ec3935208389b84d191c1fdda88843f5fa21723
Zac Wojcik <zac.wojcik@gmail.com>
2024-10-21 10:25:34 -0400

    next

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Zac Wojcik",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.className} bg-white text-black antialiased scroll-smooth`}
+    >
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-dvh">
+      <div className="m-auto">
+        <div>
+          <h1 className="text-black font-bold text-xl">Zac Wojcik</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
