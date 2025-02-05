4b8081ea2fc016138c2ae7deffab48ef9da10f1b
Zac Wojcik <zac.wojcik@gmail.com>
2025-02-05 11:31:49 -0500

    ci take 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cazcik.com",
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
