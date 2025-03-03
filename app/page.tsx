cd25678d43a834e7758c75702ab277074fed5c34
Zac Wojcik <zac.wojcik@gmail.com>
2025-03-03 10:22:51 -0500

    substack

--- app/page.tsx
+++ app/page.tsx
@@ -24,13 +24,6 @@ export default function Index() {
           <section>
             <div className="flex items-center space-x-4">
               <Link
-                href="https://x.com/cazcik"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <RiTwitterXLine className="text-black dark:text-white hover:text-neutral-700 dark:hover:text-neutral-300 size-5" />
-              </Link>
-              <Link
                 href="https://github.com/cazcik"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -38,11 +31,26 @@ export default function Index() {
                 <RiGithubLine className="text-black dark:text-white hover:text-neutral-700 dark:hover:text-neutral-300 size-5" />
               </Link>
               <Link
-                href="https://instagram.com/cazcik"
+                href="https://x.com/cazcik"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <RiInstagramLine className="text-black dark:text-white hover:text-neutral-700 dark:hover:text-neutral-300 size-5" />
+                <RiTwitterXLine className="text-black dark:text-white hover:text-neutral-700 dark:hover:text-neutral-300 size-5" />
+              </Link>
+              <Link
+                href="https://substack.com/@cazcik"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="text-black dark:text-white hover:text-neutral-700 dark:hover:text-neutral-300 size-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="fill-current"
+                  >
+                    <path d="M20 22.724l-8-4.572-8 4.572V10h16V22.724zM12 15.849l6 3.428V12H6v7.276L12 15.849zM4 6H20V8H4zM4 2H20V4H4z"></path>
+                  </svg>
+                </div>
               </Link>
               <Link
                 href="https://linkedin.com/in/cazcik"
